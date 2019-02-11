78b21531724a394b09d15c8c6dfb54ed9b5b2f06
vishnuedavarath <vishnuedavarath@gmail.com>
2019-02-11 23:51:01 +0530

    Style

--- src/components/admin/TransferList/GenPage.js
+++ src/components/admin/TransferList/GenPage.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { adminService } from "../../../__services/adminService";
 import Modal from "react-responsive-modal";
-
+import "../../../assets/allotpage/css/allotpage.css";
 export default class GenPage extends Component {
   constructor() {
     super();
     this.state = {
-      designations: ["si", "asi", "scpo","tscpo","cpo","wcpo"],
+      designations: ["si", "asi", "scpo", "tscpo", "cpo", "wcpo"],
       des: "si",
       users: [],
       open: false,
@@ -35,13 +35,13 @@ export default class GenPage extends Component {
     });
   }
   onOpenModal = e => {
-	  var tgt = JSON.parse(e.target.value);
+    var tgt = JSON.parse(e.target.value);
     this.setState({
       open: true,
       openPenno: tgt.penno,
       openName: tgt.name
-	});
-	console.log(tgt);
+    });
+    console.log(tgt);
     adminService
       .openModal(
         this.state.des,
@@ -57,8 +57,8 @@ export default class GenPage extends Component {
           op2: data.second,
           op3: data.third
         });
-	  });
-	  console.log(this.state);
+      });
+    console.log(this.state);
   };
   handleclick(e) {
     adminService
@@ -74,87 +74,110 @@ export default class GenPage extends Component {
   };
   render() {
     return (
-      <div>
-        <div>
-          <h2>General Transfer Applications</h2>
-          <span>Select Designation:</span>
-          <select
-            name="des"
-            value={this.state.opt1}
-            onChange={this.handleChange}
-          >
-            {this.state.designations.map(des => (
-              <option key={des} value={des}>
-                {des}
-              </option>
-            ))}
-          </select>
-          <button onClick={this.handleClick}>Submit</button>
-        </div>
-        <div>
-          {this.state.users.map(user => (
+      <div className="allotMainDiv">
+        <header className="profileHeader">
+          <h3 className="profileHeaderHead">Kerala Police</h3>
+          <div className="profileRight">
+            <span className="profileHeaderSpan">
+              Hello, &nbsp;&nbsp;{this.state.name}
+            </span>
+            &nbsp;&nbsp;
+            <button className="profileLogout" onClick={this.handleclick}>
+              Logout
+            </button>
+          </div>
+        </header>
+        <div className="allotContent">
+          <div className="allotDiv1">
+            <h2>General Transfer Applications</h2>
             <div>
-              <div>
-                <br />
-                _________________________________________________
-                <br />
-                <span>PEN Number:{user.penno}</span>
-                <br />
-                <span>Name:{user.name}</span>
-                <br />
-                <span>Joining Date:{user.joinDate}</span>
-                <br />
-                <button onClick={this.onOpenModal} value={JSON.stringify(user)}>
-                  Check
-                </button>
-                <br />
-                ___________________________________________________
-                <br />
-              </div>
-              <Modal open={this.state.open} onClose={this.onCloseModal} center>
-                <div>
-                  <span>Pen Number: {this.state.openPenno}</span>
-                  <br />
-                  <span>Name: {this.state.openName}</span>
-                  <br />
-                  <span>Current Station: {this.state.cur.name}</span>
-                  <br />
-                  <div>
-                    <strong>Applied Option</strong>
-                    <br />
-                    <button
-                      value={this.state.op1.id}
-                      onClick={this.handleclick}
-                    >
-                      {this.state.op1.name}
-                    </button>
-                    <br />
-                    <span>Vacancy:{this.state.op1.vacancy}</span>
-                    <br />
-                    <button
-                      value={this.state.op2.id}
-                      onClick={this.handleclick}
-                    >
-                      {this.state.op2.name}
-                    </button>
-                    <br />
-                    <span>Vacancy:{this.state.op2.vacancy}</span>
-                    <br />
-                    <button
-                      value={this.state.op3.id}
-                      onClick={this.handleclick}
-                    >
-                      {this.state.op3.name}
-                    </button>
-                    <br />
-                    <span>Vacancy:{this.state.op3.vacancy}</span>
-                    <br />
-                  </div>
-                  <button onClick={this.onCloseModal}>Close</button>
-                </div>
-              </Modal>
+              <span>Select Designation:</span>
+              <select
+                name="des"
+                value={this.state.opt1}
+                onChange={this.handleChange}
+              >
+                {this.state.designations.map(des => (
+                  <option key={des} value={des}>
+                    {des}
+                  </option>
+                ))}
+              </select>
+              <button onClick={this.handleClick}>Submit</button>
             </div>
-          ))}
+          </div>
+          <div>
+            {this.state.users.map(user => (
+              <div>
+                <div>
+                  <br />
+                  _________________________________________________
+                  <br />
+                  <span>PEN Number:{user.penno}</span>
+                  <br />
+                  <span>Name:{user.name}</span>
+                  <br />
+                  <span>Joining Date:{user.joinDate}</span>
+                  <br />
+                  <button
+                    onClick={this.onOpenModal}
+                    value={JSON.stringify(user)}
+                  >
+                    Check
+                  </button>
+                  <br />
+                  ___________________________________________________
+                  <br />
+                </div>
+                <Modal
+                  open={this.state.open}
+                  onClose={this.onCloseModal}
+                  center
+                >
+                  <div>
+                    <span>Pen Number: {this.state.openPenno}</span>
+                    <br />
+                    <span>Name: {this.state.openName}</span>
+                    <br />
+                    <span>Current Station: {this.state.cur.name}</span>
+                    <br />
+                    <div>
+                      <strong>Applied Option</strong>
+                      <br />
+                      <button
+                        value={this.state.op1.id}
+                        onClick={this.handleclick}
+                      >
+                        {this.state.op1.name}
+                      </button>
+                      <br />
+                      <span>Vacancy:{this.state.op1.vacancy}</span>
+                      <br />
+                      <button
+                        value={this.state.op2.id}
+                        onClick={this.handleclick}
+                      >
+                        {this.state.op2.name}
+                      </button>
+                      <br />
+                      <span>Vacancy:{this.state.op2.vacancy}</span>
+                      <br />
+                      <button
+                        value={this.state.op3.id}
+                        onClick={this.handleclick}
+                      >
+                        {this.state.op3.name}
+                      </button>
+                      <br />
+                      <span>Vacancy:{this.state.op3.vacancy}</span>
+                      <br />
+                    </div>
+                    <button onClick={this.onCloseModal}>Close</button>
+                  </div>
+                </Modal>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
