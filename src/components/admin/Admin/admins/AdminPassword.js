600bebc30106d600d108119f0adbb4c3ba8ae98e
vishnuedavarath <vishnuedavarath@gmail.com>
2019-02-14 23:23:00 +0530

    style

--- src/components/admin/Admin/admins/AdminPassword.js
+++ src/components/admin/Admin/admins/AdminPassword.js
@@ -7,14 +7,14 @@ export default class AdminPassword extends Component {
     this.state = {
       penno: "",
       adminId: "",
-      adminPenno:"",
-      adminName:"",
+      adminPenno: "",
+      adminName: "",
       newpassword: "",
       repassword: ""
     };
     this.handleSubmitAdminSearch = this.handleSubmitAdminSearch.bind(this);
-	this.handleChangeAdmin = this.handleChangeAdmin.bind(this);
-	this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeAdmin = this.handleChangeAdmin.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChangeAdmin(e) {
     const { name, value } = e.target;
@@ -27,60 +27,75 @@ export default class AdminPassword extends Component {
       this.setState({
         adminName: admin.admins.name,
         adminPenno: admin.admins.penno,
-        adminId: admin.admins._id,
+        adminId: admin.admins._id
       });
     });
   }
 
-  handleSubmit(e){
-	  e.preventDefault();
-    adminService.adminPassword(this.state.adminId,this.state.newpassword)
+  handleSubmit(e) {
+    e.preventDefault();
+    adminService.adminPassword(this.state.adminId, this.state.newpassword);
     window.location.reload(true);
   }
   render() {
     return (
-      <div>
-        <div>
-          <form onSubmit={this.handleSubmitAdminSearch}>
-            <input
-              type="admin"
-              name="penno"
-              value={this.state.penno}
-              onChange={this.handleChangeAdmin}
-            />
-            <button>Submit</button>
-          </form>
-        </div>
-        <div>
-          {this.state.adminId && (
-            <div>
-              <span>PEN Number:{this.state.adminPenno}</span>
-              <br />
-              <span>Name:{this.state.adminName}</span>
-              <br />
-              <h3>Change Password</h3>
-              <br />
-              <form onSubmit={this.handleSubmit}>
-                <span>New Password:</span>
+      <div className="passswordMain">
+        <header className="profileHeader">
+          <h3 className="profileHeaderHead">Kerala Police</h3>
+          <div className="profileRight">
+            <span className="profileHeaderSpan">Hello, Admin&nbsp;&nbsp;</span>
+            &nbsp;&nbsp;
+            <button className="profileLogout" onClick={this.handleclickLogout}>
+              Logout
+            </button>
+          </div>
+        </header>
+        <div className="passwordDiv">
+          <div className = "passwordSearchDiv">
+            <form onSubmit={this.handleSubmitAdminSearch}>
+              <input
+                className = "passswordInput"
+                type="admin"
+                name="penno"
+                value={this.state.penno}
+                onChange={this.handleChangeAdmin}
+              />
+              <button className = "passwordButton">Submit</button>
+            </form>
+          </div>
+          <div>
+            {this.state.adminId && (
+              <div>
+                <span>PEN Number:{this.state.adminPenno}</span>
                 <br />
-                <input
-                  type="password"
-                  name="newpassword"
-				  value={this.state.newpassword}
-				  onChange = {this.handleChangeAdmin}
-                /><br/>
-                <span>Retype New Password:</span>
+                <span>Name:{this.state.adminName}</span>
                 <br />
-                <input
-                  type="password"
-                  name="repassword"
-				  value={this.state.repassword}
-				  onChange = {this.handleChangeAdmin}
-                /><br/>
-				<button>Submit</button>
-              </form>
-            </div>
-          )}
+                <h3>Change Password</h3>
+                <br />
+                <form onSubmit={this.handleSubmit}>
+                  <span>New Password:</span>
+                  <br />
+                  <input
+                    type="password"
+                    name="newpassword"
+                    value={this.state.newpassword}
+                    onChange={this.handleChangeAdmin}
+                  />
+                  <br />
+                  <span>Retype New Password:</span>
+                  <br />
+                  <input
+                    type="password"
+                    name="repassword"
+                    value={this.state.repassword}
+                    onChange={this.handleChangeAdmin}
+                  />
+                  <br />
+                  <button>Submit</button>
+                </form>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     );
